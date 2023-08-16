a0bf55fa39a3b6302197196c478d4deb227da2f6
unknown <pandu.usahaku@gmail.com>
2023-08-16 16:07:34 +0700

    add properties to onboarding screen

--- lib/presentation/screens/onboarding_screen.dart
+++ lib/presentation/screens/onboarding_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:janganan/utils/constants/colors.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OnBoardingScreen extends StatefulWidget {
@@ -14,62 +15,83 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: PageView(
-          controller: pageController,
-          children: [
-            Center(
-              child: SizedBox(
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 30, horizontal: 20),
-                      child: Text(
-                        'Selamat Datang di Janganan',
-                        style: Theme.of(context)
-                            .textTheme
-                            .titleMedium!
-                            .copyWith(
-                                color:
-                                    Theme.of(context).colorScheme.onBackground),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 300,
-                      width: double.infinity,
-                      child: Image.asset(
-                        'assets/images/onboarding-img1.png',
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 30, horizontal: 20),
-                      child: Text(
-                        'Jaga kesehatanmu dengan memenuhi kebutuhan nutrisi harian mu dari sayur-sayuran, buah-buahan yang berkualitas.',
-                        style: Theme.of(context).textTheme.titleSmall!.copyWith(
-                            color: Theme.of(context).colorScheme.onBackground),
-                      ),
-                    ),
-                  ],
+      body: PageView(
+        controller: pageController,
+        children: [
+          // Center(
+          SizedBox(
+            height: 500,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+                  child: Text(
+                    'Selamat Datang di Janganan',
+                    style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                        color: Theme.of(context).colorScheme.onBackground),
+                    textAlign: TextAlign.start,
+                  ),
                 ),
-              ),
-            ),
-            Center(
-              child: SizedBox(
-                height: 300,
-                width: double.infinity,
-                child: Image.asset(
-                  'assets/images/onboarding-img2.png',
-                  fit: BoxFit.cover,
+                SizedBox(
+                  height: 300,
+                  width: double.infinity,
+                  child: Image.asset(
+                    'assets/images/onboarding-img1.png',
+                    fit: BoxFit.cover,
+                  ),
                 ),
-              ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+                  child: Text(
+                    'Jaga kesehatanmu dengan memenuhi kebutuhan nutrisi harian mu dari sayur-sayuran, buah-buahan yang berkualitas.',
+                    style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                        color: Theme.of(context).colorScheme.onBackground),
+                    textAlign: TextAlign.end,
+                    maxLines: 4,
+                  ),
+                ),
+              ],
             ),
-            Center(
+          ),
+          // ),
+          Center(
+            child: SizedBox(
+              height: 500,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
+                  SizedBox(
+                    height: 300,
+                    width: double.infinity,
+                    child: Image.asset(
+                      'assets/images/onboarding-img2.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Text(
+                    'Kami menyediakan sayur, buah, bumbu setiap hari.',
+                    style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                        color: Theme.of(context).colorScheme.onBackground),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Center(
+            child: SizedBox(
+              height: 500,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Selamat Berbelanja',
+                    style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                        color: Theme.of(context).colorScheme.onBackground),
+                  ),
                   SizedBox(
                     height: 300,
                     width: double.infinity,
@@ -78,20 +100,22 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
                     ),
                   ),
                   ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context)
-                          .pushReplacementNamed('/bottom-navigation');
-                    },
-                    child: Text(
-                      'MULAI',
-                      style: Theme.of(context).textTheme.titleMedium,
-                    ),
-                  ),
+                      onPressed: () {
+                        Navigator.of(context)
+                            .pushReplacementNamed('/bottom-navigation');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: AppColor.secondaryColor,
+                      ),
+                      child: Text(
+                        'MULAI',
+                        style: Theme.of(context).textTheme.titleMedium,
+                      )),
                 ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
       bottomSheet: Container(
         color: Colors.white,
