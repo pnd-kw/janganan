97fc1a0ed5dfaacbf08a649a41971056804a2a82
unknown <pandu.usahaku@gmail.com>
2023-09-06 16:15:42 +0700

    add authentication repository and bloc

--- lib/data/models/user.dart
+++ lib/data/models/user.dart
@@ -3,16 +3,24 @@ import 'package:equatable/equatable.dart';
 class User extends Equatable {
   const User({
     required this.id,
-    required this.username,
-    required this.email,
-    required this.phone,
+    this.username,
+    this.email,
+    this.phone,
+    required this.isLoggedIn,
   });
 
   final String id;
-  final String username;
-  final String email;
-  final String phone;
+  final String? username;
+  final String? email;
+  final String? phone;
+  final bool isLoggedIn;
+
+  static const empty = User(id: '', isLoggedIn: false);
+
+  bool get isEmpty => this == User.empty;
+
+  bool get isNotEmpty => this != User.empty;
 
   @override
-  List<Object> get props => [id, username, email, phone];
+  List<Object?> get props => [id, username, email, phone, isLoggedIn];
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:janganan/main.dart';
+import 'package:janganan/repository/auth_repository.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const Janganan());
+    await tester.pumpWidget(
+        Janganan(authenticationRepository: AuthenticationRepository()));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
